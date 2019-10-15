db51e26dcdd75669bb8446bce9fa6493e8da7173
mojun123 <1696384748@qq.com>
2019-10-15 16:20:06 +0800

    update

--- business/ClassManageApi.py
+++ business/ClassManageApi.py
@@ -33,8 +33,22 @@ class ClassManageApi(object):
         response = self.class_mannge_api(method,url,data=data)
         bodyDict =response.json()
         log.info("展示出的班级为{}".format(bodyDict))
-        
+        last_txt = bodyDict["retlist"][0]
+        print(last_txt)
+        intvitecode = last_txt["invitecode"]
+        id = last_txt["id"]
+        return intvitecode,id
 
+
+    def delete_add_class(self,classid):
+        method = "delete"
+        url = "http://ci.ytesting.com/api/3school/school_classes/" + str(classid)
+        log.info("删除的url：{}".format(url))
+        data = {
+            "vcode":pc.getConfValue(CONFIG_PATH, "vcode", "vcode")
+        }
+        respones = self.class_mannge_api(method,url,data=data)
+        log.info("删除成功，放回的结果为{}".format(respones))
 
 
 
@@ -59,4 +73,10 @@ classManage = ClassManageApi()
 if __name__ == "__main__":
     classManage = ClassManageApi()
     pc = ParseConfigFile()
+    ac = classManage.get_class_invitecode_and_id()
+    print(ac)
+    bc= classManage.delete_add_class(ac[1])
+    print(ac)
+    print(bc)
+    classManage.close()
 
